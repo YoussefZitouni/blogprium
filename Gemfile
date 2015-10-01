eb1a10337e7e5072562a64427a573b3f672b418e
zitoun_a <zitoun_a@epitech.eu>
2015-10-01 18:46:30 +0200

    all the project

--- Gemfile
+++ Gemfile
@@ -3,7 +3,11 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass',       '3.2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
+gem 'faker',                '1.4.2'
 gem 'bcrypt',               '3.1.7'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def new
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   # Fonction servant a voir les utilisateurs en servant de leur id like /users/1
   def show
     @user = User.find(params[:id])
@@ -56,6 +62,16 @@ class UsersController < ApplicationController
      def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+   # Confirme un utilisateur en admin
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
   end
