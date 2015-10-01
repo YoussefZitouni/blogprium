ff0bfb89441321bbf03dec3f1da0720d2912ed32
zitoun_a <zitoun_a@epitech.eu>
2015-10-01 10:12:02 +0200

    .

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+  end
+  # Fonction servant a voir les utilisateurs en servant de leur id like /users/1
+    def show
+    @user = User.find(params[:id])
+  end
+  
+    def new
+    @user = User.new
+  end
+  # Fonction servant à l'inscription, si il y a une erreur ça passe dans le else qui fait une sorte de redirection vers le template new
+   def create
+    @user = User.new(user_params)
+    if @user.save
+     
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
+end
